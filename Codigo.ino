7796a906f47f2fe1560fd263b13b1b7765d7f0c4
Miguel Catano <118161067+miguelC2004@users.noreply.github.com>
2023-11-08 00:01:05 -0500

    Create Codigo.ino

---
+++ Codigo.ino
@@ -0,0 +1,98 @@
+#include <AFMotor.h>
+
+AF_DCMotor motor1(1);
+AF_DCMotor motor2(2);
+AF_DCMotor motor3(3);
+AF_DCMotor motor4(4);
+
+char command;  // Variable para almacenar el comando recibido desde el smartphone
+
+void setup() {
+  Serial.begin(9600);
+  // Configurar el módulo Bluetooth HC-05
+  Serial.println("Ingrese el comando: ");
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    command = Serial.read();
+    motorControl(command);
+  }
+}
+
+void motorControl(char command) {
+  switch (command) {
+    case 'F':
+      forward();
+      break;
+    case 'B':
+      backward();
+      break;
+    case 'L':
+      left();
+      break;
+    case 'R':
+      right();
+      break;
+    case 'S':
+      stop();
+      break;
+    default:
+      break;
+  }
+}
+
+void forward() {
+  motor1.setSpeed(255);
+  motor1.run(FORWARD);
+  motor2.setSpeed(255);
+  motor2.run(FORWARD);
+  motor3.setSpeed(255);
+  motor3.run(FORWARD);
+  motor4.setSpeed(255);
+  motor4.run(FORWARD);
+}
+
+void backward() {
+  motor1.setSpeed(255);
+  motor1.run(BACKWARD);
+  motor2.setSpeed(255);
+  motor2.run(BACKWARD);
+  motor3.setSpeed(255);
+  motor3.run(BACKWARD);
+  motor4.setSpeed(255);
+  motor4.run(BACKWARD);
+}
+
+void left() {
+  motor1.setSpeed(200);
+  motor1.run(BACKWARD);
+  motor2.setSpeed(200);
+  motor2.run(BACKWARD);
+  motor3.setSpeed(200);
+  motor3.run(FORWARD);
+  motor4.setSpeed(200);
+  motor4.run(FORWARD);
+}
+
+void right() {
+  motor1.setSpeed(200);
+  motor1.run(FORWARD);
+  motor2.setSpeed(200);
+  motor2.run(FORWARD);
+  motor3.setSpeed(200);
+  motor3.run(BACKWARD);
+  motor4.setSpeed(200);
+  motor4.run(BACKWARD);
+}
+
+void stop() {
+  motor1.setSpeed(0);
+  motor1.run(RELEASE);
+  motor2.setSpeed(0);
+  motor2.run(RELEASE);
+  motor3.setSpeed(0);
+  motor3.run(RELEASE);
+  motor4.setSpeed(0);
+  motor4.run(RELEASE);
+}
