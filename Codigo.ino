3baa1e7cb72cafa5c620f39b29c113b437e9a2ee
Miguel Catano <118161067+miguelC2004@users.noreply.github.com>
2023-11-08 00:09:10 -0500

    Update Codigo.ino

--- Codigo.ino
+++ Codigo.ino
@@ -1,4 +1,4 @@
-#include <AFMotor.h>
+#include <AFMotor.h> //librería Adafruit Motor Shield para controlar los motores
 
 AF_DCMotor motor1(1);
 AF_DCMotor motor2(2);
@@ -8,21 +8,21 @@ AF_DCMotor motor4(4);
 char command;  // Variable para almacenar el comando recibido desde el smartphone
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(9600);// Inicializamos la comunicación serial a 9600 baudios
   // Configurar el módulo Bluetooth HC-05
   Serial.println("Ingrese el comando: ");
 }
 
 void loop() {
-  if (Serial.available() > 0) {
-    command = Serial.read();
-    motorControl(command);
+  if (Serial.available() > 0) { // Si hay datos disponibles en el puerto serial
+    command = Serial.read(); // Leemos el comando enviado desde el smartphone
+    motorControl(command); // Llamamos a la función motorControl para ejecutar el comando
   }
 }
 
 void motorControl(char command) {
   switch (command) {
-    case 'F':
+    case 'F': // Si el comando es 'F' (adelante). es lo mismo con los demás casos
       forward();
       break;
     case 'B':
@@ -43,8 +43,8 @@ void motorControl(char command) {
 }
 
 void forward() {
-  motor1.setSpeed(255);
-  motor1.run(FORWARD);
+  motor1.setSpeed(255); // Establecemos la velocidad máxima con todos los motores, y los configuramos para poder avanzar
+  motor1.run(FORWARD); 
   motor2.setSpeed(255);
   motor2.run(FORWARD);
   motor3.setSpeed(255);
@@ -54,7 +54,7 @@ void forward() {
 }
 
 void backward() {
-  motor1.setSpeed(255);
+  motor1.setSpeed(255); //estas son las funciones para retroceder
   motor1.run(BACKWARD);
   motor2.setSpeed(255);
   motor2.run(BACKWARD);
@@ -63,9 +63,9 @@ void backward() {
   motor4.setSpeed(255);
   motor4.run(BACKWARD);
 }
-
+// Configuraciones para retroceder, y moverse a lados izquierdo y derecho
 void left() {
-  motor1.setSpeed(200);
+  motor1.setSpeed(200); 
   motor1.run(BACKWARD);
   motor2.setSpeed(200);
   motor2.run(BACKWARD);
@@ -85,7 +85,7 @@ void right() {
   motor4.setSpeed(200);
   motor4.run(BACKWARD);
 }
-
+// configuración para detenerse
 void stop() {
   motor1.setSpeed(0);
   motor1.run(RELEASE);
